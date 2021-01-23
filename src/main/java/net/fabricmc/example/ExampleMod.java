1ca251edacf027cd3d12c37bdecdff5fbb768a6f
PheonixVX <TheGrimReaper.VX@gmail.com>
2021-01-22 20:47:55 -0500

    It works now

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,13 +1,27 @@
 package net.fabricmc.example;
 
+import com.mojang.minecraft.MinecraftApplet;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.entrypoint.EntrypointTransformer;
+import net.fabricmc.loader.launch.common.FabricLauncherBase;
+import java.lang.reflect.InvocationTargetException;
 
 public class ExampleMod implements ModInitializer {
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		// This is a workaround to let loader load the class.
+		try {
+			MinecraftApplet clazz = (MinecraftApplet) FabricLauncherBase.getLauncher().getTargetClassLoader().loadClass(EntrypointTransformer.appletMainClass).getDeclaredConstructor().newInstance();
+			clazz.run();
+		} catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
 
 		System.out.println("Hello Fabric world!");
 	}
